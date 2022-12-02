a768e826d4b37ecc80270480d7b2500b5d0ab404
Andres Beck-Ruiz <andres_beck-ruiz+cs1300@brown.edu>
2022-12-02 16:04:31 -0500

    Final touches

--- src/components/CompletedTrails.js
+++ src/components/CompletedTrails.js
@@ -4,11 +4,11 @@ function CompletedTrails(props) {
   return (
     <div>
       <h3>Total Distance Hiked: {Math.abs(distance).toFixed(1)}</h3>
-      <button onClick={reset}> Reset Completed </button>
       <h3> Trails Completed: </h3>
       {Object.values(trails).map((trail) => (
         <h4> {trail} </h4>
       ))}
+      <button onClick={reset}> Reset Completed </button>
     </div>
   );
 }
