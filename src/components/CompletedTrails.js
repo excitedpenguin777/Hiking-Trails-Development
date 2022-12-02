188c47af974e87adbced79efbd873189d59b52c1
Andres Beck-Ruiz <andres_beck-ruiz+cs1300@brown.edu>
2022-12-02 16:55:00 -0500

    Update lable

--- src/components/CompletedTrails.js
+++ src/components/CompletedTrails.js
@@ -3,7 +3,7 @@ function CompletedTrails(props) {
 
   return (
     <div>
-      <h3>Total Distance Hiked: {Math.abs(distance).toFixed(1)}</h3>
+      <h3>Total Distance Hiked: {Math.abs(distance).toFixed(1)} mi</h3>
       <h3> Trails Completed: </h3>
       {Object.values(trails).map((trail) => (
         <h4> {trail} </h4>
